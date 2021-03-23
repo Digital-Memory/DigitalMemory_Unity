e57218ea154904725286db8b437e57e98cc36ac4
LucasBoe <lucas@lumotex.de>
2021-03-23 09:02:54 +0100

    Added clickable button

---
+++ Assets/Scripts/Interaction/Button.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour, IClickable
+{
+    bool isClicked;
+    float clickedTimestamp;
+    float heightAnimationDuration;
+
+    [SerializeField] AnimationCurve buttonHeightOnClick;
+    [SerializeField] Transform button;
+
+    public void Click()
+    {
+        isClicked = true;
+        clickedTimestamp = Time.time;
+        heightAnimationDuration = buttonHeightOnClick[buttonHeightOnClick.length - 1].time;
+    }
+
+    public bool IsClickable()
+    {
+        return !isClicked;
+    }
+
+    private void Update()
+    {
+        if (isClicked)
+        {
+            if (Time.time > clickedTimestamp + heightAnimationDuration)
+            {
+                isClicked = false;
+            } else
+            {
+                button.transform.localPosition = new Vector3(0, buttonHeightOnClick.Evaluate(Time.time - clickedTimestamp), 0);
+            }
+        }
+    }
+}
