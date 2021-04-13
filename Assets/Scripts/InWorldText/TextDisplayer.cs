db9fb1a1725364eb59be1bb43311225057ffcfec
LucasBoe <lucas@lumotex.de>
2021-04-13 10:33:56 +0200

    optimised performance test for build

--- Assets/Scripts/InWorldText/TextDisplayer.cs
+++ Assets/Scripts/InWorldText/TextDisplayer.cs
@@ -9,84 +9,89 @@ public class TextDisplayer : MonoBehaviour
     [Expandable]
     [SerializeField]
     TextData hoverText;
+    
+    
+    [Expandable]
+    [SerializeField]
+    TextData  closeupText;
+    
+    [HideInInspector]
+    public bool HasCloseupText => closeupText != null;
+
+#if UNITY_EDITOR
 
     [Button]
     private void CreateNewHoverText()
     {
         hoverText = CreateNewTextData(gameObject.name, "_HOVER");
     }
-
-    [Expandable]
-    [SerializeField]
-    TextData  closeupText;
-
-    [HideInInspector]
-    public bool HasCloseupText => closeupText != null;
-
+    
     [Button]
     private void CreateNewCloseupText()
     {
         closeupText = CreateNewTextData(gameObject.name, "_CLOSEUP");
     }
-
+    
     private TextData CreateNewTextData(string name, string suffix)
     {
         string path = "Assets/Other/Text/";
         TextData data = TextData.CreateInstance<TextData>();
         AssetDatabase.CreateAsset(data, path + name + suffix + ".asset");
         AssetDatabase.SaveAssets();
-
+    
         return data;
     }
+
+#endif
 
     private void OnEnable()
     {
         IHoverable hoverable = GetComponent<IHoverable>();
         ICloseupable closeupable = GetComponent<ICloseupable>();
-
+    
         if (hoverable != null)
         {
             hoverable.OnStartHoverEvent += ShowHoverText;
             hoverable.OnEndHoverEvent += HideAllTexts;
         }
-
+    
         if (closeupable != null)
         {
             closeupable.OnStartCloseupEvent += ShowCloseupText;
             closeupable.OnEndCloseupEvent += HideAllTexts;
         }
     }
-
+    
     private void OnDisable()
     {
         IHoverable hoverable = GetComponent<IHoverable>();
         ICloseupable closeupable = GetComponent<ICloseupable>();
-
+    
         if (hoverable != null)
         {
             hoverable.OnStartHoverEvent -= ShowHoverText;
             hoverable.OnEndHoverEvent -= HideAllTexts;
         }
-
+    
         if (closeupable != null)
         {
             closeupable.OnStartCloseupEvent -= ShowCloseupText;
             closeupable.OnEndCloseupEvent -= HideAllTexts;
         }
     }
-
+    
     private void ShowHoverText()
     {
         if (hoverText != null)
             Game.TextDispayHandler.DisplayText(hoverText.text,transform);
     }
-
+    
     private void ShowCloseupText ()
     {
         if (closeupText != null)
             Game.TextDispayHandler.DisplayText(closeupText.text, transform);
     }
-
+    
     private void HideAllTexts()
     {
         Game.TextDispayHandler.ClearTexts();
