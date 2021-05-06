c4064c6fe84bcf0004c237e6eedc947c0d2d9ba4
Lucas Boe <lucas@lumotex.de>
2021-05-06 22:37:52 +0200

    improved workflow with photo to material loader with preview functionality and automated bundle detection

--- Assets/Scripts/AssetBundles/AddMaterialFromAssetBundleTexture.cs
+++ Assets/Scripts/AssetBundles/AddMaterialFromAssetBundleTexture.cs
@@ -1,52 +1,80 @@
 using NaughtyAttributes;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 [RequireComponent(typeof(MeshRenderer))]
 public class AddMaterialFromAssetBundleTexture : MonoBehaviour
 {
-    [ShowIf("TextureNameIsEmpty")]
-    [ValidateInput("TextureNameIsNotEmpty", "Select a texture Refenrence and Hit SetReferences.")]
+    [OnValueChanged("SetReferences")]
+    [ValidateInput("InputIsValid", "Select a texture Refenrence and Hit SetReferences.")]
     [SerializeField] Texture2D texture2D;
 
-    [ShowIf("BundleNameIsEmpty")]
-    [ValidateInput("BundleNameIsNotEmpty", "Select a bundle Refenrence and Hit SetReferences.")]
-    [Dropdown("CreateDropdown")]
-    [SerializeField] string bundle;
-
-     bool TextureNameIsEmpty() { return !TextureNameIsNotEmpty(); }
-     bool BundleNameIsEmpty() { return !BundleNameIsNotEmpty(); }
-    private bool TextureNameIsNotEmpty() { return textureName != ""; }
-    private bool BundleNameIsNotEmpty() { return bundleName != ""; }
+    private bool InputIsValid() { return textureName != "" && bundleName != ""; }
 
 
     [SerializeField] string bundleName = "";
     [SerializeField] string textureName = "";
 
-    [Button]
-
+#if UNITY_EDITOR
     private void SetReferences()
     {
         if (texture2D != null)
+        {
             textureName = texture2D.name;
 
-        if (bundle != "")
-            bundleName = bundle;
-    }
+            string path = AssetDatabase.GetAssetPath(texture2D);
 
+            bundleName = AssetDatabase.GetImplicitAssetBundleName(path);
 
-    private DropdownList<string> CreateDropdown()
-    {
-        return AssetBundleDropdownCreator.GetAssetBundleDropdown();
-    }
-
-    private void Start()
-    {
-        AddMaterial();
+            if (bundleName == "")
+            {
+                Debug.LogError("The Asset you selected seems not to be part of any bundle.");
+            }
+            else
+                Debug.Log($"update asset references path: {path} / bundle name: {bundleName}.");
+        }
     }
 
     [Button]
+    private void TestTexture () {
+        if (InputIsValid() && texture2D != null) {
+            MeshRenderer meshRenderer = GetComponent<MeshRenderer>();
+            Material material = meshRenderer.sharedMaterial;
+            material.mainTexture = texture2D;
+            meshRenderer.material = material;
+        } else {
+            Debug.LogError("Selected invalid texture for texture test.");
+        }
+    }
+
+    [Button]
+    private void ResetTexture()
+    {
+            MeshRenderer meshRenderer = GetComponent<MeshRenderer>();
+            Material material = meshRenderer.sharedMaterial;
+            material.mainTexture = null;
+           meshRenderer.material = material;
+
+    }
+
+    [Button]
+    private void AdaptWidthToHeightAndRatio() {
+
+        if (InputIsValid() && texture2D != null)
+        {
+            float height = transform.localScale.x;
+            transform.localScale = new Vector3(height, transform.localScale.y, (height / texture2D.width) * texture2D.height);
+        }
+        else
+        {
+            Debug.LogError("Selected invalid texture for width adaptation.");
+        }
+    }
+
+#endif
+
     private void AddMaterial()
     {
         Game.AssetBundleHandler.AddMaterialFromAssetBundleTexture(GetComponent<MeshRenderer>(), bundleName, textureName);
