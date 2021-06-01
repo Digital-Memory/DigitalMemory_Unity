8836963ffaf733793b9ad7f42fb2f05010cdc1c2
LucasBoe <lucas@lumotex.de>
2021-06-01 12:03:43 +0200

    small Fixes

--- Assets/Scripts/Prototyping/VideoPlayerHandler.cs
+++ Assets/Scripts/Prototyping/VideoPlayerHandler.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -35,5 +36,10 @@ public class VideoPlayerHandler : Singleton<VideoPlayerHandler>
             videoPlayer.Play();
             isLoading = false;
         }
+    }
+
+    internal void Pause(VideoWall videoWall)
+    {
+        videoPlayer.Pause();
     }
 }
