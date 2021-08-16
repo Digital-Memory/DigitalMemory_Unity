7b623872817c5188c723d6fd1b91fd85afaa1a29
LucasBoe <lucas@lumotex.de>
2021-08-16 16:31:42 +0200

    Game Start is now skipable

--- Assets/Scripts/Start/StartTextDisplayer.cs
+++ Assets/Scripts/Start/StartTextDisplayer.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class StartTextDisplayer : MonoBehaviour
 {
@@ -11,14 +12,20 @@ public class StartTextDisplayer : MonoBehaviour
     [SerializeField] StartTextConfirmer jaCustomConfirmer;
     [SerializeField] AudioSource writing;
 
+    string textToDisplay;
+    bool customConfirm;
+    private bool isSkipable;
+
     [Button]
     public void Display(string textToDisplay, bool customConfirm = false)
     {
         StopAllCoroutines();
-        StartCoroutine(DisplayRoutine(textToDisplay, customConfirm));
+        this.textToDisplay = textToDisplay;
+        this.customConfirm = customConfirm;
+        StartCoroutine(DisplayRoutine());
     }
 
-    private IEnumerator DisplayRoutine(string textToDisplay, bool customConfirm)
+    private IEnumerator DisplayRoutine()
     {
         startTextConfirmer.gameObject.SetActive(false);
         writing.Play();
@@ -28,15 +35,40 @@ public class StartTextDisplayer : MonoBehaviour
 
         for (int i = 0; i <= length; i++)
         {
-            str = textToDisplay.Substring(0, i);
-            str += "<alpha=#00>";
-            str += textToDisplay.Substring(i, length - i);
-            textBox.text = str;
-            yield return new WaitForSeconds(0.05f + Random.Range(-0.01f,0.01f));
+            isSkipable = true;
+            str = DisplayTextWithTypeEffect(length, i);
+            yield return new WaitForSeconds(0.05f + Random.Range(-0.01f, 0.01f));
         }
-        writing.Pause();
-        yield return new WaitForSeconds(0.5f);
 
+        writing.Pause();
+
+        yield return new WaitForSeconds(0.5f);
+        EnableConfirmButton();
+    }
+
+    private void EnableConfirmButton()
+    {
         (customConfirm ? (jaCustomConfirmer) : (startTextConfirmer)).gameObject.SetActive(true);
+    }
+
+    private void Update()
+    {
+        if (isSkipable && Input.GetMouseButtonDown(0))
+        {
+            StopAllCoroutines();
+            writing.Pause();
+            isSkipable = false;
+            DisplayTextWithTypeEffect(textToDisplay.Length, textToDisplay.Length);
+            EnableConfirmButton();
+        }
+    }
+
+    private string DisplayTextWithTypeEffect(int amountOfCharactersTotal, int amountOfCharactersVisible)
+    {
+        string str = textToDisplay.Substring(0, amountOfCharactersVisible);
+        str += "<alpha=#00>";
+        str += textToDisplay.Substring(amountOfCharactersVisible, amountOfCharactersTotal - amountOfCharactersVisible);
+        textBox.text = str;
+        return str;
     }
 }
