a43b41f6aaee31e1a416dfc862c7fbaf7faa7cd5
LucasBoe <lucas@lumotex.de>
2021-08-17 11:07:51 +0200

    Added Bell sound on selecting time (add it ontop of having the light bulb shine)

--- Assets/Scripts/Time/TimeMechanismLightbulb.cs
+++ Assets/Scripts/Time/TimeMechanismLightbulb.cs
@@ -1,3 +1,4 @@
+using NaughtyAttributes;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,11 +8,19 @@ public class TimeMechanismLightbulb : SimpleAttachable
     [SerializeField] MeshRenderer meshRenderer;
     [SerializeField] Material lightbulbEmissive, metal_04;
     [SerializeField] GameObject shine;
+    [SerializeField] [Foldout("Effects")] Effect onActiveEffect;
+
     public void SetLightActive(bool active)
     {
+        if (shine.activeSelf == active)
+            return;
+
         Material[] mats = meshRenderer.materials;
         mats[2] = active ? lightbulbEmissive : metal_04;
         meshRenderer.materials = mats;
         shine.SetActive(active);
+
+        if (active)
+            Game.EffectHandler.Play(onActiveEffect, gameObject);
     } 
 }
